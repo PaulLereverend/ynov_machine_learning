c0647e96a4c3602237292c5c5bcaf17db6df1a32
Paul <paulereverend@gmail.com>
2020-04-07 16:14:57 +0200

    Ajout des croisements et d'un affichage de la moyenne des fitness

--- TP1 - Sorpion/main.py
+++ TP1 - Sorpion/main.py
@@ -1,8 +1,9 @@
 
 import math
 import random
+import numpy
 
-POPULATION = 10
+POPULATION = 100
 
 VARIATION_GENES = 5/100
 
@@ -30,7 +31,7 @@ class Scorpion:
         self.longueur_fleche = longueur_fleche
         self.base_bras = base_bras
         self.hauteur_bras = hauteur_bras
-        self.note = 0
+        self.fitness = 0
 
     def ressort(self):
         return (1/3) * (self.module_young / (1 - 2 * self.coef_poisson))
@@ -50,6 +51,8 @@ class Scorpion:
         return self.masse_volumique * self.base_fleche * self.hauteur_fleche * self.longueur_fleche
 
     def velocite(self):
+        if(self.masse_proj() == 0):
+            return 0
         return math.sqrt((self.ressort() * self.longueur_deplacement() ** 2) / self.masse_proj())
 
     def portee(self):
@@ -69,7 +72,7 @@ class Scorpion:
         return (self.force_traction()*self.longueur_bras ** 3) / (48 * self.module_young * self.moment_quadratique())
 
     def is_tir(self):
-        return self.longueur_vide() < self.longueur_fleche and self.longueur_corde < self.longueur_bras
+        return self.longueur_vide() < self.longueur_fleche and self.longueur_corde < self.longueur_bras and not self.is_rupture()
 
     def is_rupture(self):
         return self.longueur_deplacement() > self.fleche_fmax()
@@ -88,35 +91,80 @@ def generate_population():
                                    Materiau.coef_poisson, random.randrange(0, 90, 1), 9.81, Materiau.masse_volumique, random.randrange(0, 100, 1)/100, random.randrange(0, 100, 1)/100, random.randrange(0, 100, 1), random.randrange(0, 100, 1), random.randrange(0, 100, 1)))
 
 
-def set_notes():
+def set_fitnesses():
     for pop in population:
         if(pop.is_tir() == True):
-            if(pop.is_rupture() == True):
-                pop.note = pop.energie_impact()*pop.portee()*2
-            else:
-                pop.note = pop.energie_impact()*pop.portee()
+            pop.fitness = pop.energie_impact()*pop.portee()*2
         else:
-            pop.note = 1
+            pop.fitness = 0
 
 
 def selection():
-    population.sort(key=lambda x: x.note, reverse=True)
-    for x in range(round(POPULATION/2), POPULATION):
-        population[x] = 0
+    population.sort(key=lambda x: x.fitness, reverse=True)
+    for x in range(round(POPULATION/2), POPULATION-1):
+        del population[round(POPULATION/2)]
 
 
 def croisements():
-    for x in range(0, round(POPULATION/2)):
-        population[x] = 0
+    probs = [i.fitness for i in population]
+    probs /= numpy.sum(probs)
+    nouvelle_population = []
+    for pop in population:
+        scorpion2 = numpy.random.choice(population, p=probs)
+        bebe_scorpion = croisement(pop, scorpion2)
+        nouvelle_population.append(bebe_scorpion)
+        while True:
+            scorpion3 = numpy.random.choice(population, p=probs)
+            if(scorpion3.fitness != scorpion2.fitness):
+                bebe_scorpion2 = croisement(pop, scorpion3)
+                if(bebe_scorpion2 not in nouvelle_population):
+                    nouvelle_population.append(bebe_scorpion2)
+                    break
+
+    return nouvelle_population
 
 
-def croisement(scorpion1: Scorpion, scorpion2: Scorpion):
+def croisement(scorpion1, scorpion2):
+    return Scorpion(scorpion1.longueur_bras, scorpion1.longueur_corde, scorpion1.module_young, scorpion1.coef_poisson, scorpion1.angle_deg, scorpion1.gravite, scorpion1.masse_volumique, scorpion2.base_fleche, scorpion2.hauteur_fleche, scorpion2.longueur_fleche, scorpion2.base_bras, scorpion2.hauteur_bras)
+
+
+def mutations():
+    for i in range(0, len(population)-1):
+        if(decision(0.5)):
+            population[i].longueur_corde += 1
+
+
+def afficher_population():
+    print('POPULATION :' + str(avg_fitness()) + ' / ' + str(max_fitness()))
+    # for pop in population:
+    # print(pop.fitness)
+
+
+def decision(probability):
+    return random.random() < probability
+
+
+def max_fitness():
+    fitnesses = [i.fitness for i in population]
+    return numpy.amax(fitnesses)
+
+
+def avg_fitness():
+    fitnesses = [i.fitness for i in population]
+    return numpy.mean(fitnesses)
 
 
 def start():
+    global population
     generate_population()
-    set_notes()
-    selection()
+    for i in range(0, 10):
+        set_fitnesses()
+        selection()
+        population = croisements()
+        set_fitnesses()
+        mutations()
+        afficher_population()
+    # afficher_population()
 
 
 start()
